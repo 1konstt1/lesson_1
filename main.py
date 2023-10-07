3c4c03f4d4c59b31068a7aac73927dfeb728cef6
1konstt1 <1konstt1@gmail.com>
2023-10-07 14:13:16 +0300

    31-40

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print("Система расчёта штрафов d Германии")
 
-car_speed = 147
-is_town = True
+car_speed = 51
+is_town = False
+
 
 fine_for_town_1_to_10 = 30
 fine_for_town_11_to_15 = 50
@@ -13,6 +14,7 @@ fine_for_town_41_to_50 = 400
 fine_for_town_51_to_60 = 560
 fine_for_town_61_to_70 = 700
 fine_for_town_70_and_more = 800
+
 
 fine_for_country_1_to_10 = 20
 fine_for_country_11_to_15 = 40
@@ -36,6 +38,27 @@ else:
 
 if over_speed < 1:
   print("Скорость не превышена или превышена незначительно")
+elif over_speed >= 1 and over_speed <= 10 and is_town: # 1 - 10
+  print("Штраф: " + str(fine_for_town_1_to_10) + " Евро") 
 elif over_speed >= 1 and over_speed <= 10:
-  print("Штраф: " + str(fine_for_town_1_to_10) )
-  
+  print("Штраф: " + str(fine_for_country_1_to_10) + " Евро")
+elif over_speed >= 11 and over_speed <= 15 and is_town: # 11 - 15
+  print("Штраф: " + str(fine_for_town_11_to_15) + " Евро") 
+elif over_speed >= 11 and over_speed <= 15:
+  print("Штраф: " + str(fine_for_country_11_to_15) + " Евро") 
+elif over_speed >= 16 and over_speed <=20 and is_town: # 16 - 20
+  print("Штраф: " + str(fine_for_town_16_to_20) + " Евро") 
+elif over_speed >= 16 and over_speed <=20:
+  print("Штраф: " + str(fine_for_country_16_to_20))
+elif over_speed >= 21 and over_speed <=25 and is_town: # 21 - 25
+  print("Штраф: " + str(fine_for_town_21_to_25))
+elif over_speed >= 21 and over_speed <=25:
+  print("Штраф: " + str(fine_for_country_21_to_25))
+elif over_speed >= 26 and over_speed <=30 and is_town: # 26 - 30
+  print("Штраф: " + str(fine_for_town_26_to_30))
+elif over_speed >= 26 and over_speed <= 30:
+  print("Штраф: " + str(fine_for_country_26_to_30))
+elif over_speed >= 31 and over_speed <=40 and is_town: # 31 - 40
+  print("Штраф: " + str(fine_for_town_31_to_40))
+elif over_speed >= 31 and over_speed <=40:
+  print("Штраф: " + str(fine_for_country_31_to_40))
